210335e73c96f707ec10d2145f21f06d4e608c66
Isaac <isaacw1925@gmail.com>
2024-06-21 17:34:09 -0700

    Day 1: Sidebar UI + misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import stake_icon from "./assets/white_stake.webp"
+import { Blind } from './components/Blind'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div id="sidebar">
+        <Blind />
+
+        <div id="round-score">
+          <div id="round-score-text">Round<br/>score</div>
+          <div id="score-display">
+            <img id="stake-icon" src={stake_icon} />
+            <div id="score">0</div>
+          </div>
+        </div>
+
+        <div id="hand-info">
+          <div id="hand-type">
+            <div id="hand-name">High Card</div>
+            <div id="hand-level">lvl.1</div>
+          </div>
+          <div id="calculator">
+            <div id="chips">5</div>
+            <div id="X">X</div>
+            <div id="mult">1</div>
+          </div>
+        </div>
+
+        <div id="info">
+          <div id="buttons">
+            <div id="run-info"><b>Run</b><br/>Info</div>
+            <div id="options">Options</div>
+          </div>
+          <div id="stats">
+            <div id="hands-discards">
+              <div id="hands-box" className="box">
+                Hands
+                <div id="hands">4</div>
+              </div>
+              <div id="discards-box" className="box">
+                Discards
+                <div id="discards">3</div>
+              </div>
+            </div>
+            <div id="money-box">
+              <div id="money">$21</div>
+            </div>
+            <div id="run-stats">
+              <div id="ante-box" className="box">
+                Ante
+                <div id="ante-display">
+                  <div id="ante">2</div>/&nbsp;8
+                </div>
+              </div>
+              <div id="round-box" className="box">
+                Round
+                <div id="round">3</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Blind.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import small_blind from "../assets/small_blind.webp"
+import stake_icon from "../assets/white_stake.webp"
+import "./Blind.css"
+
+export const Blind = () => {
+  return (
+    <div id="blind">
+        <div id="blind-name">Small Blind</div>
+        <div id="blind-info">
+            <div id="blind-bio"></div>
+            <div id="blind-display">
+                <img id="blind-icon" src={small_blind} />
+                <div id="blind-status">
+                    <div id="blind-score-text">Score at least</div>
+                    <div id="req-display">
+                        <img id="stake-icon" src={stake_icon} />
+                        <div id="req-score">800</div>
+                    </div>
+                    <div id="reward-display">
+                        Reward:
+                        <div id="blind-reward">$$$</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
