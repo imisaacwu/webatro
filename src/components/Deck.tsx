a9f2158f8c7c81ecb329aea78c65b7a6fde04cf3
Isaac <isaacw1925@gmail.com>
2024-07-25 13:03:15 -0700

    Deck image fix

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -5,6 +5,12 @@ import { GameStateContext } from '../GameState'
 import { DeckType } from '../Constants'
 const decks: Record<string, { default: string }> = import.meta.glob('../assets/decks/*.png', { eager: true })
 
+const getDeck = (deck: string) => {
+    const module = decks[`../assets/decks/${deck}.png`]
+    if(!module) { throw new Error(`no such image ${deck}.png`) }
+    return module.default
+}
+
 export const Deck = () => {
     const { state: game } = useContext(GameStateContext)
     const [ menuActive, setMenuActive ] = useState(false)
@@ -17,7 +23,7 @@ export const Deck = () => {
             />
             <div id='deck-area' className='card-area'>
                 <div id='face-down' onClick={() => setMenuActive(true)}>
-                    <img src={decks[`../assets/decks/${DeckType[game.stats.deck]}.png`].default} />
+                    <img src={getDeck(DeckType[game.stats.deck])} />
                 </div>
             </div>
             <div id='deck-label' className='counter'>{game.cards.deck.length}/{game.cards.deck.length + game.cards.hand.length + game.cards.submitted.length + game.cards.hidden.length}</div>
